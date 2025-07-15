4def74a9cc7c74c5496b887b9b036b9d468fe57c
elidvenega <robotsmailNinja@gmail.com>
2025-07-15 15:06:28 -0700

    Worked on Expo project

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,6 +1,18 @@
+import { Image } from "expo-image";
+import { ImageSourcePropType, StyleSheet } from "react-native";
 
-export default function ImageViewer() {
-  return (
-    <div>ImageViewer</div>
-  )
+type Props = {
+    imgSource: ImageSourcePropType
 }
+
+export default function ImageViewer({ imgSource}: Props) {
+  return <Image source={imgSource} style={styles.image} />
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    }
+})
